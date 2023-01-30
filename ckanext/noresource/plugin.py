4a4c36878c229e439a09d6253c14b9051caf6acd
miloshIra <milosh.aritonski@keitaro.com>
2023-01-30 12:05:17 +0100

    Initial commit

---
+++ ckanext/noresource/plugin.py
@@ -0,0 +1,19 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+from ckanext.noresource.views.dataset import noresource_dataset
+
+class NoresourcePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
+    
+    # IConfigurer
+
+    def get_blueprint(self):
+        return [noresource_dataset]
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets',
+            'noresource')
